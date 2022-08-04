5cd4aeffb1f304a9326f6094d04d92eb5a1d71a6
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2022-08-04 01:33:30 +0100

    50%

--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -1,35 +1,58 @@
 import React from "react";
-import { Container, Image } from "react-bootstrap";
+import "../styles/landing.css";
+import { Button, Container, Image } from "react-bootstrap";
 import CVs from "../assets/CVs.png";
-import Fade from "react-reveal/Fade";
+import { Fade } from "react-reveal";
 const LandingPage = () => {
+  const screenHeight = window.screen.availHeight + "px";
+  const screenWidth = window.screen.availWidth + "px";
+
   return (
-    <div>
+    <div id="landing" className="py-3" style={{ height: screenHeight }}>
       <Fade right>
         <Image
           fluid
           src={CVs}
           className="me-3 img-fluid d-none d-lg-block "
           style={{
-            position: "fixed",
+            position: "absolute",
             zIndex: "-1",
             top: "0",
-            right: 0,
-            opacity: "0.8",
-            transform: "rotate(25deg)",
+            marginTop: "40px",
+            right: "0",
+            marginRight: "-1000px",
           }}
         />
       </Fade>
       <Container className="pt-5">
-        <div className="text-start">
-          <Fade left>
-            <p className="display-1 fw-bolder text-start mt-5">Assistancia</p>
-            <p className="fs-1 text-start">
-              Job applications have never been easier
+        <Fade left>
+          <div className="text-start">
+            <p
+              style={{ font: "Gill Sans" }}
+              className="display-1 fw-bolder text-lg-start text-sm-center mt-5"
+            >
+              Assistancia
             </p>
-          </Fade>
-          
-        </div>
+            <p className="fs-2 text-lg-start text-sm-center">
+              Job applications have <span className="text-danger">never</span>{" "}
+              been easier !
+            </p>
+            <p className="fs-6 text-lg-start text-sm-center">
+              A simple click is what it takes to create whatever document you
+              may need to land your dream job.
+            </p>
+            <div className="text-lg-start text-sm-center">
+              <Button
+                className="mt-4"
+                variant="primary"
+                size="lg"
+                style={{ backgroundColor: "#4cbb17" }}
+              >
+                Explore
+              </Button>
+            </div>
+          </div>
+        </Fade>
       </Container>
     </div>
   );
