759bf890032ab3a927c4fa07aa21fd91334a6358
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2022-08-06 12:42:53 +0100

    CHANGES

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingPage from './pages/landingpage';
+import OurMission from './pages/ourmission';
+import WhyUs from './pages/whyus';
+import MadeBy from './pages/madeby';
 
 function App() {
   return (
     <div className="App">
       <LandingPage/>
+      <OurMission />
+      <WhyUs />
+      <MadeBy />
     </div>
   );
 }
