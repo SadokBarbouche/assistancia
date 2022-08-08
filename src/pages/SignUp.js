4142ff00f719ee1ccb23b29ab978cd47752017d3
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2022-08-08 14:59:30 +0100

    more fixes

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -5,102 +5,115 @@ import { Fade } from "react-reveal";
 
 const SignUp = () => {
   return (
-    <div>
-      <Container className="my-5" style={{}}>
-        <Row>
-          <p
-            style={{ color: "rgb(3, 4, 94)" }}
-            className="display-2 text-start fw-bolder"
-          >
-            If opportunity doesn't knock,{" "}
-            <span
-              style={{ color: "#00B4D8", borderBottom: "5px #00B4D8 solid " }}
-            >
-              build
-            </span>{" "}
-            a door.
-          </p>
-          <Col className="text-start fw-bolder">
-            <Form>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
+    <Fade>
+      <div style={{
+        backgroundColor:"#90E0EF",minHeight:"100vh",
+}}>
+        <div style={{}}>
+          <Container style={{}}>
+            <Row>
+              <p
+                style={{ color: "rgb(3, 4, 94)" }}
+                className="display-2 text-start fw-bolder"
               >
-                <Form.Label className="fs-5">Email address:</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
-              </Form.Group>
-
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label className="fs-5">Password:</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Type your password"
-                />
-              </Form.Group>
-
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label className="fs-5">Confirm your password:</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Retype your password"
-                />
-              </Form.Group>
-
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label className="fs-5">Birthday:</Form.Label>
-                <Form.Control type="date" placeholder="" />
-              </Form.Group>
-
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label className="fs-5">Upload your profile picture:</Form.Label>
-                <Form.Control type="file" placeholder="" />
-              </Form.Group>
-              <div
-                className="mt-4"
-                style={{ display: "flex", justifyContent: "center" }}
-              >
-                <Button
-                  type="submit"
+                If opportunity doesn't knock,{" "}
+                <span
                   style={{
-                    borderRadius: "12px",
-                    backgroundColor: "rgb(3, 4, 94)",
+                    color: "#00B4D8",
+                    borderBottom: "5px #00B4D8 solid ",
                   }}
-                  className=""
                 >
-                  Create new account
-                </Button>
-              </div>
-            </Form>
-          </Col>
-          <Col className="d-none d-lg-block">
-              <Image
-                fluid
-                style={{
-                  transform: "scaleX(-1)",
-                  zIndex: "-1",
-                  marginLeft: "-500px",
-                  position: "absolute",
-                  marginTop: "-40px",
-                  height:"75%"
-                }}
-                src={SignUpPic}
-              />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+                  build
+                </span>{" "}
+                a door.
+              </p>
+              <Col className="text-start fw-bolder">
+                <Form>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label className="fs-5">Email address:</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                  </Form.Group>
+
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label className="fs-5">Password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Type your password"
+                    />
+                  </Form.Group>
+
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label className="fs-5">
+                      Confirm your password:
+                    </Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Retype your password"
+                    />
+                  </Form.Group>
+
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label className="fs-5">Birthday:</Form.Label>
+                    <Form.Control type="date" placeholder="" />
+                  </Form.Group>
+
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label className="fs-5">
+                      Upload your profile picture:
+                    </Form.Label>
+                    <Form.Control type="file" placeholder="" />
+                  </Form.Group>
+                  <div
+                    className="mt-4"
+                    style={{ display: "flex", justifyContent: "center" }}
+                  >
+                    <Button
+                      type="submit"
+                      style={{
+                        borderRadius: "12px",
+                        backgroundColor: "rgb(3, 4, 94)",
+                      }}
+                      className=""
+                    >
+                      Create new account
+                    </Button>
+                  </div>
+                </Form>
+              </Col>
+              <Col className="d-none d-lg-block">
+                <Image
+                  fluid
+                  style={{
+                    transform: "scaleX(-1)",
+                    zIndex: "-1",
+                    marginLeft: "-500px",
+                    position: "absolute",
+                    marginTop: "-40px",
+                    height: "75%",
+                  }}
+                  src={SignUpPic}
+                />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    </Fade>
   );
 };
 export default SignUp;
--- src/pages/login.js
+++ src/pages/login.js
@@ -4,90 +4,99 @@ import LoginImage from "../assets/login.svg";
 import Footer from "../components/Footer.js";
 import Blob from "../assets/blob.svg";
 import { NavLink } from "react-router-dom";
-import "../styles/login.css";
+// import "../styles/login.css";
 import { Fade } from "react-reveal";
 const Login = () => {
   return (
-    <div style={{}}>
-      <Container className="text-start" style={{ backgroundColor: "#90E0EF" }}>
-        <Row
-          className="py-5"
-          style={{ marginTop: window.screen.availHeight * 0.1 + "px" }}
+      <div
+        style={{
+          minHeight:"100vh",
+          padding:"50px",
+          backgroundColor: "rgb(144, 224, 239)",
+        }}
+      >
+        <Container
+          className="text-start"
+          style={{ backgroundColor: "#90E0EF" }}
         >
-          <Col style={{ backgroundColor: "#90E0EF" }}>
-            <Fade left>
-              <p
-                className="display-5 fw-bold mb-4"
-                style={{ color: "#03045E" }}
-              >
-                Opportunities don't happen until{" "}
-                <span
-                  style={{
-                    color: "#00B4D8",
-                    borderBottom: "5px #00B4D8 solid",
-                    paddingBottom: "5px",
-                  }}
+          <Row
+            className="py-5"
+            // style={{ marginTop: window.screen.availHeight * 0.1 + "px" }}
+          >
+            <Col style={{ backgroundColor: "#90E0EF" }}>
+              <Fade left>
+                <p
+                  className="display-5 fw-bold mb-4"
+                  style={{ color: "#03045E" }}
                 >
-                  you
-                </span>{" "}
-                create them.
-              </p>
-              <Form style={{ backgroundColor: "#90E0EF" }}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label className="fs-4 fw-bold">
-                    Email address
-                  </Form.Label>
-                  <Form.Control
-                    type="email"
-                    className=""
-                    placeholder="Enter email"
-                  />
-                  <Form.Text className="text-muted"></Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label className="fs-4 fw-bolder">Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Stay Logged" />
-                </Form.Group>
-                <Button
-                  className="me-2"
-                  style={{
-                    display: "inline",
-                    borderRadius: "15px",
-                    backgroundColor: "#03045E",
-                  }}
-                  type="submit"
-                >
-                  Log In
-                </Button>
-                <NavLink to="/SignUp" activeStyle>
-                  <p
-                    className=" text-decoration-underline fw-bold"
+                  Opportunities don't happen until{" "}
+                  <span
                     style={{
-                      display: "inline",
                       color: "#00B4D8",
+                      borderBottom: "5px #00B4D8 solid",
+                      paddingBottom: "5px",
                     }}
                   >
-                    Don't have an account ? Sign up !
-                  </p>
-                </NavLink>
-              </Form>
-            </Fade>
-          </Col>
-          <Col
-            style={{ backgroundColor: "#90E0EF", zIndex: "" }}
-            className="d-none d-xl-block"
-          >
-            <Fade right>
-              <Image src={LoginImage} style={{ marginTop: "15px" }} fluid />
-            </Fade>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+                    you
+                  </span>{" "}
+                  create them.
+                </p>
+                <Form style={{ backgroundColor: "#90E0EF" }}>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label className="fs-4 fw-bold">
+                      Email address
+                  </Form.Label>
+                    <Form.Control
+                      type="email"
+                      className=""
+                      placeholder="Enter email"
+                    />
+                    <Form.Text className="text-muted"></Form.Text>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label className="fs-4 fw-bolder">Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Stay Logged" />
+                  </Form.Group>
+                  <Button
+                    className="me-2"
+                    style={{
+                      display: "inline",
+                      borderRadius: "15px",
+                      backgroundColor: "#03045E",
+                    }}
+                    type="submit"
+                  >
+                    Log In
+                  </Button>
+                  <NavLink to="/SignUp" activeStyle>
+                    <p
+                      className=" text-decoration-underline fw-bold"
+                      style={{
+                        display: "inline",
+                        color: "#00B4D8",
+                      }}
+                    >
+                      Don't have an account ? Sign up !
+                    </p>
+                  </NavLink>
+                </Form>
+              </Fade>
+            </Col>
+            <Col
+              style={{ backgroundColor: "#90E0EF", zIndex: "" }}
+              className="d-none d-xl-block"
+            >
+              <Fade right>
+                <Image src={LoginImage} style={{ marginTop: "15px" }} fluid />
+              </Fade>
+            </Col>
+          </Row>
+        </Container>
+      </div>
   );
 };
 export default Login;
